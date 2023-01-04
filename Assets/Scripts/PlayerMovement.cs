840d86b5848869813abaab55835d56d913d27f07
elliott dombrowski <elliottroyal@me.com>
2023-01-03 23:35:37 -0600

    setup sprite x y flipping

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -18,6 +18,7 @@ public class PlayerMovement : MonoBehaviour
   void Update()
   {
     Run();
+    FlipSprite();
   }
 
   void OnMove(InputValue value)
@@ -30,5 +31,16 @@ public class PlayerMovement : MonoBehaviour
   {
     Vector2 playerVelocity = new Vector2 (moveInput.x * runSpeed, rb.velocity.y); // KEEP CURRENT VELOCITY ON Y
     rb.velocity = playerVelocity;
+  }
+
+  void FlipSprite()
+  {
+    // GET ABSOLUTE VALUE OF X - TRUE IF GREATER THAN EPSILON - AKA GREATER THAN 0 TO A FINE DECIMAL LEVEL.
+    bool playerHasHorizontalSpeed = Mathf.Abs(rb.velocity.x) > Mathf.Epsilon;
+
+    if (playerHasHorizontalSpeed)
+    {
+      transform.localScale = new Vector2 (Mathf.Sign(rb.velocity.x), 1f);
+    }
   }
 }
